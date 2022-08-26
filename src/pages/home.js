081979dc3f88f36a7ddf8efee85dc185bc19836e
Yifei <ai8hyf@gmail.com>
2022-08-26 12:09:44 -0400

    create project
    react
    react-router

---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+export default function Home() {
+    return (
+      <>
+        <main>
+          <h2>Welcome to the homepage!</h2>
+          <p>You can do this, I believe in you.</p>
+        </main>
+        <nav>
+          <a href="/about">About</a>
+        </nav>
+      </>
+    );
+}
---
+++ src/router.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="about" element={<About />} /> */}
+        </Routes>
+    );
+}
