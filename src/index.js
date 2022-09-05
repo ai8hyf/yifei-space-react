5866026f6f0d8a915fe285f02054143f938fb482
Yifei <ai8hyf@gmail.com>
2022-09-04 21:26:33 -0400

    router fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./router.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
   </BrowserRouter>
 );
